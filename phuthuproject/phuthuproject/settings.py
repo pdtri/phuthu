903ca761d8d0fafe6e16b73cd889c54aade51bfa
phan duc tri <pdtri@cantho.gov.vn>
2017-10-16 13:05:03 +0700

    ds

--- phuthuproject/phuthuproject/settings.py
+++ phuthuproject/phuthuproject/settings.py
@@ -118,3 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static','static_dirs') # chu y load css
+
+STATICFILES_DIRS=(
+                  os.path.join(BASE_DIR,'static','static_root'),
+				  #/Users/ADMIN/OneDrive/hoclamweb/django/qldd/qldd/static/static_root/,
+)
