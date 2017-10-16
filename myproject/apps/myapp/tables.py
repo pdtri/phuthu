cb626bf7256eac0cbd0de75574b0f9ab07398a72
phan duc tri <pdtri@cantho.gov.vn>
2017-10-16 12:55:23 +0700

    ds

--- myproject/apps/myapp/tables.py
+++
@@ -1,8 +0,0 @@
-# tutorial/tables.py
-import django_tables2 as tables
-from apps.myapp.models import Congtac
-class CongtacTable(tables.Table):
-    class Meta:
-        model = Congtac
-        #add class="paleblue" to <table> tag
-        attrs = {'class': 'paleblue'}
--- myproject/apps/myapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-from . import views_congtac
-urlpatterns = [
-    url(r'^add_congtac/$', views_congtac.add_congtac, name='add_congtac'),
-    url(r'^tim_congtac/$', views_congtac.tim_congtac.as_view(), name='tim_congtac'),
-]
