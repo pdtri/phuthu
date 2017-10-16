b17a6d4ab5f36c33813b0f8c6e1cf24b6ffbed22
phan duc tri <pdtri@cantho.gov.vn>
2017-10-17 06:34:26 +0700

    dsds

--- phuthuproject/phuthuproject/settings.py
+++ phuthuproject/phuthuproject/settings.py
@@ -157,7 +157,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static_dirs') # chu y load css
+STATIC_ROOT = os.path.join(BASE_DIR,'static','static_dirs') # chu y load css
 
 STATICFILES_DIRS=(
                   os.path.join(BASE_DIR,'static','static_root'),
