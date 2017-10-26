afac940710779fefa695d412cbcb4828682d8564
phan duc tri <pdtri@cantho.gov.vn>
2017-10-26 14:14:08 +0700

    sdsd

--- phuthuproject/static/djng/thu.js
+++ phuthuproject/static/djng/thu.js
@@ -3,6 +3,7 @@ var myApp = angular.module('myApp').config(function($interpolateProvider) {
     $interpolateProvider.startSymbol('{$');
     $interpolateProvider.endSymbol('$}');
 });
+
 myApp.controller("nhapCtrl", ['$scope', '$http', '$cookies',
  function ($scope, $http, $cookies) {
     $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
@@ -55,31 +56,52 @@ myApp.controller("nhapCtrl", ['$scope', '$http', '$cookies',
 
 //myApp.controller("MyFormCtrl", ['$scope', '$http', '$cookies',
 
+            /*
+             * Author: thehalfheart@gmail.com
+             * Website: freetuts.net
+             */
+var myTaps=  angular.module('ngTabExample', [])
+
+myApp.controller('TabController', function($scope){
+                    // $scope index lưu trữ tab hiện tại, ban đầu gán nó = tab1
+                    $scope.current_tab = 1;
+
+                    // Hàm đổi tab
+                    $scope.changeTab = function(index){
+                        $scope.current_tab = index;
+                    };
+                    // Hàm kiểm tra có phải tab hiện tại hay không
+                    // hàm này sẽ trả về true/false và dùng kết hợp
+                    // với ng-class để active menu
+                    $scope.isActiveTab = function(index){
+                        return index === $scope.current_tab;
+                    };
+                });
 myApp.controller("MyFormCtrl",
-                                                         ['$scope', '$http', '$cookies',
-                                                              function ($scope, $http, $cookies) {
-                                                                       $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-                                                                        // To send the csrf code.
-                                                                       $http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
-                                                                       // This function is called when the form is submitted.
-                                                                       $scope.submit = function ($event) {
-                                                                                       // Prevent page reload.
-                                                                              $event.preventDefault();
-                                                                                      // Send the data.
-                                                                              var in_data = jQuery.param({
-                                                                                                 'id': $scope.congtac.id,
-																								 'ten': $scope.congtac.ten,
-                                                                                                 'diachi': $scope.congtac.diachi,
-                                                                                                 'congtac': $scope.congtac.congtac,
-                                                                                                 'csrfmiddlewaretoken': $cookies.csrftoken
-                                                                                                 });
-                                                                               $http.post('/add', in_data)
-                                                                                           .success(function(out_data) {
+                              ['$scope', '$http', '$cookies',
+                function ($scope, $http, $cookies) {
+                                          $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+                                             // To send the csrf code.
+                                          $http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
+                                             // This function is called when the form is submitted.
+                                          $scope.submit = function ($event) {
+                                             // Prevent page reload.
+                                          $event.preventDefault();
+                                              // Send the data.
+                                          var in_data = jQuery.param({
+                                              'id': $scope.congtac.id,
+											  'ten': $scope.congtac.ten,
+                                              'diachi': $scope.congtac.diachi,
+                                              'congtac': $scope.congtac.congtac,
+                                              'csrfmiddlewaretoken': $cookies.csrftoken
+                                                                });
+                                           $http.post('/add', in_data)
+                                               .success(function(out_data) {
                                                                                            // Reset the form in case of success.
-                                                                                                $scope.card = angular.copy({});
+                                                     $scope.card = angular.copy({});
                                                                                             });
-                                                                       }; //het lenh scope
-                                                         }]);
+                                                     }; //het lenh scope
+                                             }]);
 
 
 
@@ -97,16 +119,3 @@ myApp.controller("PanelController",
               });// het control
 
 
-function openCity(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
