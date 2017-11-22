24f556c9c951db1bc74d2d7209a33d709c0491d7
phan duc tri <pdtri@cantho.gov.vn>
2017-11-22 21:31:09 +0700

    fsfd

--- phuthuproject/apps/chuyenthue/admin.py
+++ phuthuproject/apps/chuyenthue/admin.py
@@ -162,8 +162,8 @@ def to_pdf(modeladmin, request, queryset):
         #response['Content-Disposition'] = 'filename=\"thu.pdf"'.format(1)
         #chuoi= 'D:\hoclamweb\myfolderproject\phuthuproject\static\css\inphieu.css'
         BASE_DIR = os.path.dirname((__file__))
-        S = (os.path.join(BASE_DIR, 'static'))
-        chuoi = str(s)+'/css/inphieu.css'
+        thu = (os.path.join(BASE_DIR, 'static'))
+        chuoi = str(thu)+'/css/inphieu.css'
         weasyprint.HTML(string=html).write_pdf(response, stylesheets=[weasyprint.CSS(chuoi)])
         return response
         #contex = json.dumps(data_row)
